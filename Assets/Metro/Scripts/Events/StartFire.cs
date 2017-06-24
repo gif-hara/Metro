816fc299fe7b191421fa8ba2984f33b44ec015ef
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-06-25 00:56:42 +0900

    :new: StartFire追加

---
+++ Assets/Metro/Scripts/Events/StartFire.cs
@@ -0,0 +1,11 @@
+﻿using HK.Framework.EventSystems;
+
+namespace Metro.Events.Character
+{
+    /// <summary>
+    /// 発射開始イベント
+    /// </summary>
+    public sealed class StartFire : UniRxEvent<StartFire>
+    {
+    }
+}
