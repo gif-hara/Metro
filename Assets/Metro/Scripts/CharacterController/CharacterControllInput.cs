d3bf0a860a21b0deab0f29a9ba2a886793c32bde
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-06-24 20:01:12 +0900

    :new: 移動できるように

---
+++ Assets/Metro/Scripts/CharacterController/CharacterControllInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets._2D;
+
+namespace Metro.CharacterController
+{
+    [RequireComponent(typeof(PlatformerCharacter2D))]
+    public class CharacterControllInput : MonoBehaviour
+    {
+        [SerializeField]
+        private string horizontalName = "Horizontal";
+
+        [SerializeField]
+        private string verticalName = "Vertical";
+
+        private PlatformerCharacter2D character;
+
+        void Awake()
+        {
+            this.character = GetComponent<PlatformerCharacter2D>();
+            Assert.IsNotNull(this.character);
+        }
+    
+        void Update()
+        {
+            var horizontal = CrossPlatformInputManager.GetAxis(this.horizontalName);
+            this.character.Move(horizontal, false, false);
+        }
+    }
+}
