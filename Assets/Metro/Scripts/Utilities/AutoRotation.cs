9c4131e2889cb256540723f46b85423a1f317fad
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-06-24 23:56:54 +0900

    :new: プロペラが回転する

---
+++ Assets/Metro/Scripts/Utilities/AutoRotation.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Metro.Utilities
+{
+    public class AutoRotation : MonoBehaviour
+    {
+        public enum AxisType
+        {
+            X = 0,
+            Y = 1,
+            Z = 2,
+        }
+
+        [SerializeField]
+        private AxisType axisType;
+        
+        [SerializeField]
+        private float speed;
+
+        private Vector3 cachedDirection;
+
+        private Transform cachedTransform;
+
+        void Awake()
+        {
+            switch (this.axisType)
+            {
+                case AxisType.X:
+                    this.cachedDirection = Vector3.right;
+                    break;
+                case AxisType.Y:
+                    this.cachedDirection = Vector3.up;
+                    break;
+                case AxisType.Z:
+                    this.cachedDirection = Vector3.forward;
+                    break;
+            }
+
+            this.cachedTransform = this.transform;
+        }
+
+        void Update()
+        {
+            this.cachedTransform.localRotation *= Quaternion.AngleAxis(this.speed * Time.deltaTime, this.cachedDirection);
+        }
+    }
+}
