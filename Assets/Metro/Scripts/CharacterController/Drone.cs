37629e68c67d407c6de55ab5c0e94699e27396c9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-06-25 00:34:54 +0900

    :new: モデルを生成出来るように

--- Assets/Metro/Scripts/CharacterController/Drone.cs
+++ Assets/Metro/Scripts/CharacterController/Drone.cs
@@ -7,8 +7,12 @@ namespace Metro.CharacterController
         [SerializeField]
         private GameObject model;
 
+        private Transform cachedTransform;
+
         void Awake()
         {
+            this.cachedTransform = this.transform;
+            var modelInstance = Instantiate(this.model, this.cachedTransform);
         }
     }
 }
