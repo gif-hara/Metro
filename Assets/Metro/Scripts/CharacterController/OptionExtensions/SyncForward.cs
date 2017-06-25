4f38870b5c17ae22b12c5f339a110dfc996e06b7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-06-25 11:24:20 +0900

    :new: 滑らかに回転するように

--- Assets/Metro/Scripts/CharacterController/OptionExtensions/SyncForward.cs
+++ Assets/Metro/Scripts/CharacterController/OptionExtensions/SyncForward.cs
@@ -6,16 +6,19 @@ namespace Metro.CharacterController.OptionExtensions
 {
     public sealed class SyncForward : OptionExtension
     {
-        private readonly Vector3 FixedEuler = new Vector3(0.0f, -90.0f, 0.0f);
-        
+        private readonly float Sensitivity = 10.0f;
+
+        private float angle;
+
         public override void Created(Drone drone, Option option)
         {
             drone.Provider.Receive<Move>()
                 .Where(m => drone.isActiveAndEnabled)
-                .SubscribeWithState(option, (m, _option) =>
+                .SubscribeWithState2(this, option, (m, _this, _option) =>
                 {
-                    _option.CachedTransform.rotation = Quaternion.LookRotation(m.Direction, Vector3.right);
-                    _option.CachedTransform.rotation *= Quaternion.Euler(FixedEuler);
+                    var target = Mathf.Atan2(m.Direction.y, m.Direction.x) * Mathf.Rad2Deg;
+                    _this.angle = Mathf.LerpAngle(_this.angle, target, Sensitivity * Time.deltaTime);
+                    _option.CachedTransform.rotation = Quaternion.AngleAxis(_this.angle, Vector3.forward);
                 })
                 .AddTo(this);
         }
