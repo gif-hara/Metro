cfe134c4f5239d299fca99d5b95f744afe72f9f7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-07-01 17:30:42 +0900

    :new: SetTargetFrameRate追加

---
+++ Assets/Metro/Scripts/Systems/SetTargetFrameRate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Metro.Systems
+{
+    public sealed class SetTargetFrameRate : MonoBehaviour
+    {
+        [SerializeField]
+        private int targetFrameRate;
+        
+        void Awake()
+        {
+            Application.targetFrameRate = targetFrameRate;
+        }
+    }
+}
