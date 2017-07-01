a4c77c346cbd68545a98715386556af0ad1a4672
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-07-01 16:00:17 +0900

    :new: InputSystems実装中

---
+++ Assets/Metro/Scripts/InputSystems/InputSystemViewer.cs
@@ -0,0 +1,41 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Metro.InputSystems
+{
+    public sealed class InputSystemViewer : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform region;
+        
+        [SerializeField]
+        private RectTransform pointer;
+
+        private Vector2 cachedRegionSize;
+
+        void Awake()
+        {
+            var anchorSize = this.region.anchorMax - this.region.anchorMin;
+            this.cachedRegionSize.Set(anchorSize.x * Screen.width, anchorSize.y * Screen.height);
+            Debug.Log(this.cachedRegionSize);
+            this.UpdateAsObservable()
+                .Where(_ => this.isActiveAndEnabled)
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    _this.Begin(Input.mousePosition);
+                });
+        }
+
+        void Start()
+        {
+        }
+
+        public void Begin(Vector2 screenPosition)
+        {
+            this.pointer.anchoredPosition = screenPosition;
+        }
+        
+    }
+}
---
+++ Assets/Metro/Scripts/InputSystems/TapViewer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Metro.InputSystems
+{
+    public sealed class TapViewer : MonoBehaviour, IPointerDownHandler
+    {
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Debug.Log("PointerDown");
+        }
+    }
+}
