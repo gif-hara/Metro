0231c76466ebc75ca6e0959257889dad40173e0a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-07-04 01:09:18 +0900

    :new: 狙うのをやめる挙動を実装

--- Assets/Metro/Scripts/CharacterController/HumanoidAnimationController.cs
+++ Assets/Metro/Scripts/CharacterController/HumanoidAnimationController.cs
@@ -1,4 +1,5 @@
-﻿using Metro.Events.Character;
+﻿using DG.Tweening;
+using Metro.Events.Character;
 using UniRx;
 using UnityEngine;
 using UnityEngine.Assertions;
@@ -13,6 +14,8 @@ namespace Metro.CharacterController
         private CharacterLocomotion locomotion;
         
         private Animator animator;
+
+        private Tweener unAimTweener = null;
 
         private static class AnimatorParameter
         {
@@ -46,6 +49,19 @@ namespace Metro.CharacterController
                 {
                     var layerIndex = _this.animator.GetLayerIndex("Aim Layer");
                     _this.animator.SetLayerWeight(layerIndex, 1.0f);
+                    if (_this.unAimTweener != null)
+                    {
+                        _this.unAimTweener.Kill();
+                    }
+                    _this.unAimTweener = DOTween.To(
+                        () => 1.0f,
+                        f =>
+                    {
+                        _this.animator.SetLayerWeight(layerIndex, f);
+                    },
+                        0.0f,
+                        0.5f)
+                        .SetDelay(1.0f);
                 })
                 .AddTo(this);
                 
