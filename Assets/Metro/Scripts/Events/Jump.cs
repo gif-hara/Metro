7175b33b76e47e48cfb8e240af3b2cf58533dded
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-07-04 22:58:33 +0900

    :new: ジャンプイベント追加

---
+++ Assets/Metro/Scripts/Events/Jump.cs
@@ -0,0 +1,10 @@
+﻿using HK.Framework.EventSystems;
+using UnityEngine;
+
+namespace Metro.Events.Character
+{
+    public sealed class Jump : UniRxEvent<Jump, Vector2>
+    {
+        public Vector2 Direction { get { return this.param1; } }
+    }
+}
