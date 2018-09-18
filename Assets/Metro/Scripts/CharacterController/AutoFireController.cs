86ca722406763eecaa05815c5de79f9ac25c6145
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-09-18 21:58:37 +0900

    :new: 自動で攻撃するシステムを構築中

---
+++ Assets/Metro/Scripts/CharacterController/AutoFireController.cs
@@ -0,0 +1,22 @@
+﻿using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UniRx;
+
+namespace Metro
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class AutoFireController : MonoBehaviour
+    {
+        void Start()
+        {
+            this.OnTriggerEnter2DAsObservable()
+                .Subscribe(c =>
+                {
+                    Debug.Log(c.name);
+                });
+        }
+    }
+}
